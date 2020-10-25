15b644979d6b2f5b75a59bee050f820fbf98d22b
Malina228 <71219885+Malina228@users.noreply.github.com>
2020-10-25 22:06:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number === 0)
+        return 'zero';
+    
+    let result = '';
+    let str = String(number);
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let teen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    if (number > 99) {
+        result += ones[str[str.length - 3]] + ' hundred ';
+        str = str.slice(1);
+    }
+
+    if (str > 19) {
+        result += tens[str[str.length - 2]] + ' ' + ones[str[str.length - 1]];
+        return result.trim();
+    }
+
+    if (str > 9) {
+        return result += teen[str[str.length - 1]];
+    }
+
+    result += ones[str[str.length - 1]];
+
+    return result.trim();
 }
